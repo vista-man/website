eaad353cab1a4fc05fdd358a8351f4f1cd567a11
vista-man <524715@vistacollege.nl>
2024-10-17 09:06:41 +0200

    nigga

--- scripts.js
+++ scripts.js
@@ -1,28 +1,16 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+import "./styles.css";
+let index = 0;
+displayImages();
+function displayImages() {
+ let i;
+ const images = document.getElementsByClassName("image");
+ for (i = 0; i < images.length; i++) {
+   images[i].style.display = "none";
+ }
+ index++;
+ if (index > images.length) {
+   index = 1;
+ }
+ images[index-1].style.display = "block";
+ setTimeout(displayImages, 2000); 
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
